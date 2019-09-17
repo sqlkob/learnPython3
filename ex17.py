ed229c097722bb9514eed57aaa9bc07e5e3f6199
Kob <Matt.Kob@coxautoinc.com>
2019-09-17 10:11:59 -0400

    final testing done

--- ex17.py
+++ ex17.py
@@ -10,3 +10,15 @@ in_file = open(from_file)
 indata = in_file.read()
 
 print(f"The input file is {len(indata)} bytes long")
+
+print(f"Does the output file exist? {exists(to_file)}")
+print("ready, hit RETURN to continure, CTRL-C to about.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()
